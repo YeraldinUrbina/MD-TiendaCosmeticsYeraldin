3b1f250f2de5f2a8119345f097d9083c37671041
Yeraldin Urbina <yeriisurbina54@gmail.com>
2024-10-09 18:01:13 -0500

    Actualizando

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="home-container">
       <div className="home-content">
-        <h1 className='bienvenido'>Bienvenido a la Mejor Tienda de Comesticos</h1>
+        <h1 className='bienvenido'>Bienvenido a la Mejor Tienda de Cosmeticos</h1>
         <p>¡Explora nuestros productos increíbles y disfruta de las mejores ofertas!</p>
       </div>
     </div>
