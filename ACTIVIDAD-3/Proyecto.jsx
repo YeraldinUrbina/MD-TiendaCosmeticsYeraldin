5415cf6f5a362edf995ce26750d2106c9e6c78b5
Yeraldin Urbina <yeriisurbina54@gmail.com>
2024-10-09 20:10:20 -0500

    Actividad 3

---
+++ ACTIVIDAD-3/Proyecto.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+      <div>
+        <h1>Yeraldin Urbina</h1>  {/* Reemplaza 'Tu Nombre Completo' por tu nombre real */}
+      </div>
+    );
+  }
+  
+  export default App;
